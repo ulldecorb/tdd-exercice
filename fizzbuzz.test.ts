ae3792f16c445f8b46186c491e0ff7d082a9b6e9
ulldecorb <ausias.bertran@gmail.com>
2022-02-17 13:32:31 +0100

    Add eslint library

--- fizzbuzz.test.ts
+++ fizzbuzz.test.ts
@@ -1,12 +1,12 @@
-const fizzbuzz = require('./fizzbuzz');
+const fizzbuzz = require('./fizzbuzz.ts');
 
-describe('fizzbuzz', () =>  {
-    test('test is run',() => {
-        expect(true).toBe(true);
-    });
-    test('input 1 return 1', () => {
-        const expected = fizzbuzz(1);
-        const returned = 1;
-        expect(expected).toBe(returned);
-    })
-})
+describe('fizzbuzz', () => {
+  test('test is run', () => {
+    expect(true).toBe(true);
+  });
+  test('input 1 return 1', () => {
+    const expected = fizzbuzz(1);
+    const returned = 1;
+    expect(expected).toBe(returned);
+  });
+});
